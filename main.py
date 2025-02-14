7d8dbac6c8806b2473e49971269e707be51ed1c5
escalon <escalon@Escalon.escalon>
2025-02-14 13:52:36 -0500

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.reconnaissance.shodan_recon import ShodanHandler
 from src.reconnaissance.dir_enum import Application as dApp
 import argparse
 from colorama import Fore, Style
+from tabulate import tabulate
 
 def main():
     try:
@@ -26,10 +27,20 @@ def main():
         banner = pyfiglet.figlet_format("SpiderScan", font="slant")
         print(f"{Fore.RED}{banner}{Style.RESET_ALL}")
 
-        print(f"{Fore.RED}[1] reconnaissance\t\t\t [2] scanning{Style.RESET_ALL}")
-        print(f"{Fore.RED}[+] banner grabber \t\t\t [+] nmap scan\n[+] directory enumeration\t\t [+] SQL injection\n[+] DNS enumeration\n[+] Domain and subdomain enumeration\n[+] Header recon\n[+] Shodan recon\n[+] whois recon\n[+] js file analyzer\n[+] Wayback scraper\n[+] Google dork\n{Style.RESET_ALL}")
-
-
+        data = [
+            [f"{Fore.GREEN}[+] Banner Grabber", f"{Fore.GREEN}[+] Nmap Scan"],
+            [f"{Fore.GREEN}[+] Directory Enumeration", f"{Fore.GREEN}[+] SQL Injection"],
+            [f"{Fore.GREEN}[+] DNS Enumeration", f"{Fore.GREEN}[+] CSRF testing"],
+            [f"{Fore.GREEN}[+] Domain & Subdomain Enumeration", f"{Fore.GREEN}[+] API Testing"],
+            [f"{Fore.GREEN}[+] Header Recon", f"[+] Broken Authentication and Session Security Tester"],
+            [f"{Fore.GREEN}[+] Shodan Recon", f"[+] XSS test"],
+            [f"{Fore.GREEN}[+] WHOIS Recon",f"[+] Insecure direct object reference(IDOR) test" ],
+            [f"{Fore.GREEN}[+] JS File Analyzer", ""],
+            [f"{Fore.GREEN}[+] Wayback Scraper", ""],
+            [f"{Fore.GREEN}[+] Google Dork", ""],
+            [f"{Fore.GREEN}[+] Website status",""]
+        ] 
+        print(tabulate(data, headers=[f"{Fore.CYAN}[i] Reconnaissance", f"{Fore.CYAN}[i] Scanning"], tablefmt="grid"))
         opt = input('Choose an option: ') 
 
     except KeyboardInterrupt as ke:
