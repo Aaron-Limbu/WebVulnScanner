0795773bf204fd4c5301cec3a7238461a949d71f
Aaron-Limbu <aaronlimbu381@gmail.com>
2025-03-11 22:04:04 +0545

    ongoing

--- src/interfaces/dash.py
+++ src/interfaces/dash.py
@@ -9,6 +9,7 @@ class Dash(ctk.CTk):
         self.grid_columnconfigure(0, weight=0)
         self.grid_columnconfigure(1,weight=1)
         self.grid_rowconfigure(0,weight=1)
+        self.content_frame=None
         self.show_sidebar()
         self.show_content()
     
@@ -18,30 +19,113 @@ class Dash(ctk.CTk):
         self.sidebar.grid_propagate(False)
         label1 = ctk.CTkLabel(self.sidebar,text="Overview",font=("Arial",20),anchor="w",compound="left")
         label1.grid(row=0,column=0,padx=10,pady=10,sticky="ew")
-        home_btn = ctk.CTkButton(self.sidebar,text="Home",fg_color="transparent",anchor="w",compound="left",font=("Arial",15))
+        home_btn = ctk.CTkButton(self.sidebar,text="Home",fg_color="transparent",anchor="w",compound="left",font=("Arial",15),corner_radius=8,hover_color="#1e1e1e",command=self.show_content)
         home_btn.grid(row=1,column=0,padx=10,pady=10)
-        UH_btn = ctk.CTkButton(self.sidebar,text="Programmes",fg_color="transparent",anchor="w",compound="left",font=("Arial",15))
+        UH_btn = ctk.CTkButton(self.sidebar,text="Programmes",fg_color="transparent",anchor="w",compound="left",font=("Arial",15),corner_radius=8,hover_color="#1e1e1e",command=self.show_programmes)
         UH_btn.grid(row=2,column=0,padx=10,pady=10)
         label2 = ctk.CTkLabel(self.sidebar,text="Logging",font=("Arial",20),anchor="w",compound="left")
         label2.grid(row=3,column=0,padx=10,pady=10,sticky="ew")
-        log_btn = ctk.CTkButton(self.sidebar,text="URL Log",fg_color="transparent",anchor="w",compound="left",font=("Arial",15))
+        log_btn = ctk.CTkButton(self.sidebar,text="URL Log",fg_color="transparent",anchor="w",compound="left",font=("Arial",15),corner_radius=8,hover_color="#1e1e1e")
         log_btn.grid(row=4,column=0,padx=10,pady=10)
-        log_btn2 = ctk.CTkButton(self.sidebar,text="Export Log",fg_color="transparent",anchor="w",compound="left",font=("Arial",15))
+        log_btn2 = ctk.CTkButton(self.sidebar,text="Export Log",fg_color="transparent",anchor="w",compound="left",font=("Arial",15),corner_radius=8,hover_color="#1e1e1e")
         log_btn2.grid(row=5,column=0,padx=10,pady=10)
+        label3 = ctk.CTkLabel(self.sidebar,text="Settings",font=("Arial",20),anchor="w",compound="left")
+        label3.grid(row=6,column=0,padx=10,pady=10,sticky="ew")
+        toggl_btn = ctk.CTkButton(self.sidebar,text="Log out",fg_color="transparent",anchor="w",compound="left",font=("Arial",15),corner_radius=8,hover_color="#1e1e1e")
+        toggl_btn.grid(row=7,column=0,padx=10,pady=10)
         # label3 = ctk.CTkLabel(self.sidebar,text="Settings",font=("Arial",20),anchor="w",compound="left")
         # label3.grid(row=6,column=0,padx=10,pady=10,sticky="ew")
         # toggl_btn = ctk.CTkButton(self.sidebar,text="Log out",fg_color="transparent",anchor="w",compound="left",font=("Arial",15))
         # toggl_btn.grid(row=7,column=0,padx=10,pady=10)
 
     def show_content(self): 
-        self.content_frame = ctk.CTkFrame(self,corner_radius=10,fg_color="#1e1e1e")
-        self.content_frame.grid(row=0,column=1,sticky="nsew",padx=20,pady=20)
-        self.content_label = ctk.CTkLabel(self.content_frame,text="hello",font=("Arial",20))
-        self.content_label.grid(row=0,column=0,padx=10,pady=10)
-        
-
-
-
+        if self.content_frame: 
+            self.content_frame.destroy()
+        self.content_frame = ctk.CTkFrame(self, corner_radius=10, fg_color="#1e1e1e")
+        self.content_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
+        self.content_frame.grid_columnconfigure(0, weight=1) 
+        title_label = ctk.CTkLabel(self.content_frame,text="About this Application",font=("Arial",40,"bold"),text_color="white")
+        title_label.grid(row=0,column=0,padx=10,pady=(20,5),sticky="n")
+        sub_tlabel = ctk.CTkLabel(self.content_frame,text="Identify Web Vulnerabilities (OWASP TOP 10)",font=("Arial",20),text_color="#AAAAAA")
+        sub_tlabel.grid(row=1,column=0,padx=10,pady=0,sticky="n")
+        owasp_frame = ctk.CTkFrame(self.content_frame,fg_color="#252525",corner_radius=10)
+        owasp_frame.grid(row=2,column=0,padx=20,pady=10,sticky="ew")
+        owasp_text=ctk.CTkLabel(owasp_frame,
+                                text="The OWASP Top 10 is a standard awareness document for developers and security professionals."
+                                "It represents the most critical security risks to web applications.",
+                                font=("Arial",25,),
+                                text_color="white",
+                                wraplength=800,
+                                justify="left"
+                                )
+        owasp_text.pack(padx=10,pady=15)
+        owasp_details = [
+            (
+                "1. Injection",
+                "Injection Flaws, such as SQL, NoSQL and XSS occur when untrusted data is sent to a server, allowing attackers to execute unauthorized commands and access data."
+            ),
+            (
+                "2 Broken Authentication",
+                "Weak authentication mechanism allow attackers to compromise user crendentials, leading to unauthorize access, identity theft and system control."
+            ),
+            (
+                "3 Sensitive Data Exposure",
+                "Applications that fail to protect sensitive data(e.g. passwords, credit card info) using encryption or proper access controls leave data vulnerable to breaches"
+            ),
+            (
+                "4 Security Misconfiguration",
+                "Improper security settings, such as default credentials, open directories, or exposed debug modes, can lead to system compromises."
+            ),
+            (
+                "5 Cross-Site Scripting (XSS)",
+                "XSS vulnerabilities allow attackers to inject malicious scripts into web pages, affecting users by stealing credentials or modifying site content."
+            ),
+            (
+                "6 Insecure Deserialization",
+                "Flaws in deserialization allow attackers to execute remote code, escalate privileges, or cause denial-of-service attacks."
+            ),
+            (
+                "7 Using Components with known vulnerabilities",
+                "Libraries, frameworks and software modules with known security flaws can be exploited if not updated or patched."
+            ),
+            (
+                "8 Insufficient Logging and Monitoring",
+                "Lack of proper logging and real-time monitoring makes it difficult to detect breaches and respond effectively to security incidents."
+            )
+        ]
+        owasp_frame2 = ctk.CTkScrollableFrame(self.content_frame,fg_color="transparent")
+        owasp_frame2.grid(row=3,column=0,padx=20,pady=10,sticky="nsew")
+        owasp_frame2.grid_columnconfigure(0, weight=1)
+        self.content_frame.grid_rowconfigure(3,weight=1)
+        for i,(title,description) in enumerate(owasp_details): 
+            title_label = ctk.CTkLabel(
+                owasp_frame2,
+                text=title,
+                font=("Arial",20,"bold"),
+                text_color="#FFFFFF"
+            )
+            title_label.grid(row=i*2,column=0,padx=10,pady=(10,2),sticky="w")
+            desc_label = ctk.CTkLabel(
+                owasp_frame2,
+                text=description,
+                font=("Arial",15),
+                text_color="#DDDDDD",
+                wraplength=900,
+                justify="left"
+            )
+            desc_label.grid(row=i*2+1,column=0,padx=15,pady=(0,10),sticky="w")
+    
+    def show_programmes(self): 
+        if self.content_frame: 
+            self.content_frame.destroy()
+        self.content_frame = ctk.CTkFrame(self, corner_radius=10, fg_color="#1e1e1e")
+        self.content_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
+        title_label = ctk.CTkLabel(self.content_frame,text="Programmes",font=("Arial",40,"bold"),text_color="white")
+        title_label.grid(row=0,column=0,padx=30,pady=(20,5),sticky="n")
+        sub_tlabel = ctk.CTkLabel(self.content_frame,text="List of Ongoing programmes",font=("Arial",20),text_color="#AAAAAA")
+        sub_tlabel.grid(row=1,column=0,padx=30,pady=0,sticky="n")
+                
+    
 
 if __name__ == "__main__": 
     try: 
