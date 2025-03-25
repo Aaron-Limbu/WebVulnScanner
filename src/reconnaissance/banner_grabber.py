848c425283c8ea39c39c5c01138beb2f11d42afb
Aaron-Limbu <aaronlimbu381@gmail.com>
2025-03-26 02:34:59 +0545

    code

--- src/reconnaissance/banner_grabber.py
+++ src/reconnaissance/banner_grabber.py
@@ -1,6 +1,19 @@
 import socket
+import logging
+import os
 import argparse
+class Logger:
+    @staticmethod
+    def setup_logger(log_filename):
 
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            handlers=[
+                logging.FileHandler(log_filename),
+                logging.StreamHandler()  # Optionally log to console as well
+            ]
+        )
 
 class BannerGrabber:
     def __init__(self, target, ports):
@@ -15,26 +28,38 @@ class BannerGrabber:
                 s.sendall(b"HEAD / HTTP/1.1\r\n\r\n")
                 banner = s.recv(1024).decode().strip()
                 print(f"[+] Banner for {ip}:{port}:\n{banner}\n")
+                logging.info(f"[+] Banner for {ip}:{port}:\n{banner}\n")
                 return banner
         except (socket.timeout, socket.error) as e:
             print(f"[-] Unable to grab banner for {ip}:{port}: {e}")
+            logging.info(f"[-] Unable to grab banner for {ip}:{port}: {e}")
             return None
 
     def run(self):
         try:
+            # log_path = f"{os.getcwd()}\\logs\\banner_grabber.log"  # Set the correct log path
+            # Logger.setup_logger("banner.log")  # Set up the logger with the correct file path
+
             ip = socket.gethostbyname(self.target)
             print(f"[+] Target resolved to IP: {ip}")
             print("[+] Starting banner grabbing...")
+            logging.info(f"[+] Target resolved to IP: {ip}")
+            logging.info("[+] Starting banner grabbing...")
 
             for port in self.ports:
                 print(f"[i] Scanning port {port}...")
+                logging.info(f"[i] Scanning port {port}...")
                 self.grab_banner(ip, port)
 
             print("[+] Banner grabbing completed.")
+            logging.info("[+] Banner grabbing completed.")
         except socket.gaierror as e:
             print(f"[-] Error resolving hostname {self.target}: {e}")
+            logging.error(f"[-] Error resolving hostname {self.target}: {e}")
         except Exception as e:
             print(f"[-] Unexpected error: {e}")
+            logging.error(f"[-] Unexpected error: {e}")
+
 
 
 if __name__ == "__main__":
